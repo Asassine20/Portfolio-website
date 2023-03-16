b4d3f6be22e52969e55f33a39210fb1ccf804719
Asassine20 <122495785+Asassine20@users.noreply.github.com>
2023-03-15 21:36:58 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controls');
+const secBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+function PageTransitions(){
+  //button click active class
+}
