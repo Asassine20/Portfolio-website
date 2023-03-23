adc46d3335d054c4aeb00aa480801fdd1ec42be1
Asassine20 <122495785+Asassine20@users.noreply.github.com>
2023-03-22 21:41:12 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const sections = document.querySelectorAll('.section');
-const secBtns = document.querySelectorAll('.controls');
-const secBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
 
 function PageTransitions(){
   //button click active class
+  for(let i = 0; i < sectBtn.length; i++){
+    sectBtn[i].addEventListener('click', function(){
+      let currentBtn = document.querySelectorAll('.active-btn');
+      currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+      this.className += ' active-btn';
+      })
+  }
+  //sections active class
+  allSections.addEventListener('click', (e)=>{
+    const id = e.target.dataset.id;
+    if(id){
+      sectBtns.forEach((btn) =>{
+        btn.classList.remove('active')
+      })
+      e.target.classList.add('active')
+
+      sections.forEach((section) =>{
+        section.classList.remove('active')
+      })
+
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  })
 }
+PageTransitions();
+
